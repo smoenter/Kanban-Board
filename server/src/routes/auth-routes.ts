7c9739ae6eb2b86b136ca0c02d096625d82d613a
smoenter <staceymoenter@gmail.com>
2025-03-01 16:47:48 -0500

    Updated code to auth-routes and middleware auth.ts

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -3,38 +3,30 @@ import { User } from '../models/user.js';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'; //added lines 6&7. import .env file
 dotenv.config();
 
+//added lines 11-30
 export const login = async (req: Request, res: Response) => {
-  console.log(req.body);
   // TODO: If the user exists and the password is correct, return a JWT token
   const { username, password } = req.body;
-
   const user = await User.findOne({
     where: { username },
   });
-
   if (!user) {
     return res.status(401).json({ message: 'Authentication failed' });
   }
-
   const passwordIsValid = await bcrypt.compare(password, user.password);
   if (!passwordIsValid) {
     return res.status(401).json({ message: 'Authentication failed' });
   }
-
   const secretKey = process.env.JWT_SECRET_KEY || '';
-
-  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
-
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '30s' });
   return res.json({ token });
-
-};
-
+}
 const router = Router();
-router.post("/test", (_req,res)=> res.status(200).json("test Route"))
 // POST /login - Login a user
 router.post('/login', login);
+
 
 export default router;
